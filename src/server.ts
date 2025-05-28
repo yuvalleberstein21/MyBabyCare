80e3d369647d68d0a7c173cb04c2f7b71f5057ce
MailBIT Sales <yuvalleberstein21@gmail.com>
2025-05-29 02:35:29 +0300

    error Handler and middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,28 @@
-import express from "express";
-import dotenv from "dotenv";
+import express, { ErrorRequestHandler } from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.send("Baby Tracker API is running!");
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (req, res) => {
+  res.send('MyBabyCare API is running!');
 });
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(`${req.method}: ${req.originalUrl}, failed with error ${err}`);
+  next(err);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT} `);
 });
