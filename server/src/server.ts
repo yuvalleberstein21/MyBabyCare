a40e356a740a34d07da718753fd45687606ef3dd
MailBIT Sales <yuvalleberstein21@gmail.com>
2025-12-04 21:36:07 +0200

    update

--- server/src/server.ts
+++ server/src/server.ts
@@ -31,14 +31,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
-// const limiter = rateLimit({
-//   windowMs: 15 * 60 * 1000, // 15 minutes
-//   limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-//   standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-//   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-// });
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+});
 
-// app.use(limiter);
+app.use(limiter);
 
 // Swagger APIS
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
