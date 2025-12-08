128d4e63c1ff21cbf4c418642cd7cc2c58236a1e
MailBIT Sales <yuvalleberstein21@gmail.com>
2025-12-08 14:24:50 +0200

    update with name cheap domain

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,7 +22,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['https://my-baby-care-tau.vercel.app', 'http://localhost:5173'],
+    origin: [
+      'https://my-baby-care-tau.vercel.app',
+      'https://app.babytracker.online/',
+      'http://localhost:5173',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
